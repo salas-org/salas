7be81532e4e51081be686a55289a0c8e48acb8a1
paularmand <paularmand.verhaegen@gmail.com>
2021-10-20 17:32:50 +0200

    added scripts to get and patch ethereum code

---
+++ install/1_clean_code.sh
@@ -0,0 +1,15 @@
+
+source ../conf/0_load_conf.sh
+
+# need write on all directories for the deletes to succeed
+read -p "This will remove all patched ethereum for this node from your system. Do you want to continue? y/n [n] " yn
+case $yn in
+    [Yy]* ) ;;
+    [Nn]* ) exit;;
+    * ) exit;;
+esac
+
+# removing data from chain
+echo "removing $ETHPATH"
+chmod -R 777 $ETHPATH
+rm -rf $ETHPATH
---
+++ install/2_get_ethereum_and_patch_and_compile.sh
@@ -0,0 +1,14 @@
+
+source ../conf/0_load_conf.sh
+
+# removing data from chain
+echo "pulling geth (go-ethereum) to $ETHPATH"
+git clone $GETH_REPO $ETHPATH/src
+
+# patch
+patch $ETHPATH/src/consensus/ethash/consensus.go < ./consensus.patch
+patch $ETHPATH/src/params/protocol_params.go < ./protocol_params.patch
+
+# compile
+cd $ETHPATH/src
+go install ./...
