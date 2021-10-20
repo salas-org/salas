c5474e5ddd2f326ccc3df23ac875f98c2bf81f54
paularmand <paularmand.verhaegen@gmail.com>
2021-10-20 17:28:07 +0200

    added scripts to easily start a bootnode

---
+++ bootnode/3_start_node.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# load configuration
+source ../conf/0_load_conf.sh
+
+LOCAL_ETH_DATA_PATH=$(pwd)/ethereum_data
+LOCAL_SALAS_CONF_PATH=$(pwd)/salas_conf
+
+VERBOSITY=3
+LOCAL_PORT=30313
+EXTRA_FLAGS="--nodiscover"
+
+# run a mining node with an IPC via file geth.ipc in data directory 
+echo "*******"
+echo 'starting a node with this command:'
+node_cmd="$GOPATH/bin/geth --nat none --datadir $LOCAL_ETH_DATA_PATH --syncmode full --verbosity $VERBOSITY --port $LOCAL_PORT --networkid $NETWORKID"
+node_cmd="$node_cmd $EXTRA_FLAGS"
+echo $node_cmd
+$($node_cmd)
